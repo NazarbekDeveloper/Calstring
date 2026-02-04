ced6decf1699b235aea3cfc50b023ac5cd40ff31
NazarbekDeveloper <bekzalatoy05@gmail.com>
2026-02-04 18:38:44 +0500

    Kod metodlar qo'shib qaytadan yozildi

--- Program.cs
+++ Program.cs
@@ -8,29 +8,14 @@ do
     Console.WriteLine("1. Oddiy bo'lim              2. Pro bo'lim");
     Console.WriteLine("1) qo'shish (+)              1) darajaga");
     Console.WriteLine("2) ayirish  (-)              2) kvadrat ildiz");
-    Console.WriteLine("3) ko'paytirish  (-)         3) logarifm");
-    Console.WriteLine("4) bo'lish  (-)              4) faktorial\n");
+    Console.WriteLine("3) ko'paytirish  (*)         3) logarifm");
+    Console.WriteLine("4) bo'lish  (/)              4) faktorial\n");
     Console.Write("Bo'limni tanlang >> ");
     int bolimTanla = int.Parse(Console.ReadLine());
     Console.WriteLine();
     if (bolimTanla == 1)
     {
-        Console.Write("birinchi son: a = ");
-        double a = double.Parse(Console.ReadLine());
-
-        Console.Write("amalni kiriting(+ , - , * , /): >> ");
-        char amal = char.Parse(Console.ReadLine());
-        Console.Write("ikkinchi son: b = ");
-        double b = double.Parse(Console.ReadLine());
-        if (amal == '+') Console.WriteLine($"{a} {amal} {b} = {a + b}");
-        else if (amal == '-') Console.WriteLine($"{a} {amal} {b} = {a - b}");
-        else if (amal == '*') Console.WriteLine($"{a} {amal} {b} = {a * b}");
-        else if (amal == '/')
-        {
-            if (b == 0) Console.WriteLine("Sonni 0 ga bo'lsih mumkun emas");
-            else Console.WriteLine($"{a} {amal} {b} = {a / b}");
-        }
-        else Console.WriteLine("Bunday amal majud emas!!!");
+        OddiyAmallarBajar();
     }
     else if (bolimTanla == 2)
     {
@@ -40,50 +25,19 @@ do
         int amal = int.Parse(Console.ReadLine());
         if (amal == 1)
         {
-            Console.Write("LET'S GO!!! a ning b darajasini hisoblaymiz \n");
-            Console.Write("a = ");
-            double a = double.Parse(Console.ReadLine());
-            Console.Write("b = ");
-            double b = double.Parse(Console.ReadLine());
-            Console.WriteLine($"{a}^{b} = {Math.Pow(a, b)}");
+            DarajagaKotar();
         }
         else if (amal == 2)
         {
-            Console.Write("LET'S GO!!! a ning kvadrat ildizini topamiz\n");
-            Console.Write("a = ");
-            double a = double.Parse(Console.ReadLine());
-            if (a < 0) Console.WriteLine("Manfiy sonning kvadrat ildizini topish mumkun emas!!!");
-            Console.WriteLine($"{a} ning kvadrat ildizi {Math.Sqrt(a)} ga teng");
+            IldizTop();
         }
         else if (amal == 3)
         {
-            Console.Write("LET'S GO!!! Logarif a asosga ko'ra b ni hisoblaymiz \n");
-            Console.Write("a = ");
-            double a = double.Parse(Console.ReadLine());
-            Console.Write("b = ");
-            double b = double.Parse(Console.ReadLine());
-            if (a > 0 && a != 1 && b > 0) Console.WriteLine($"log {a} asosga ko'ra {b} ning natijasi {Math.Log(b) / Math.Log(a)} ga teng");
-            else Console.WriteLine("Siz xato son kiritdingiz!!!\na va b nomanfiy sonlar va (a != 1) bo'lishi kerak");
+            LogarifmHisobla();
         }
         else if (amal == 4)
         {
-            Console.Write("Qaysi sonning faktorialini hisoblaymiz..?\n a = ");
-            int a = int.Parse(Console.ReadLine());
-            int P = 1;
-            if (a < 0) Console.Write("Manfiy sonning faktorialini hisoblash mumkun emas!!!");
-            else
-            {
-                if (a == 0) Console.WriteLine("0! = 1");
-                else
-                {
-                    for (int i = 1; i <= a; i++)
-                    {
-                        P *= i;
-                    }
-                    Console.WriteLine($"{a} faktorial {P} ga teng , ya'ni: {a}! = {P}");
-                }
-
-            }
+            FaktorialHisobla();
         }
         else Console.WriteLine("Bunday amal xattoki PRO Max calculyatorda ham yo'qku bratim...!!!");
     }
@@ -95,3 +49,70 @@ do
     qaytaIshlat = int.Parse(Console.ReadLine());
 } while (qaytaIshlat == 1);
 Console.WriteLine("SuperCalculyator dasturidan foydalanganiz uchun Thank You Bro...!!!");
+
+static void OddiyAmallarBajar()
+{
+    Console.Write("birinchi son: a = ");
+    double a = double.Parse(Console.ReadLine());
+
+    Console.Write("amalni kiriting(+ , - , * , /): >> ");
+    char amal = char.Parse(Console.ReadLine());
+    Console.Write("ikkinchi son: b = ");
+    double b = double.Parse(Console.ReadLine());
+    if (amal == '+') Console.WriteLine($"{a} {amal} {b} = {a + b}");
+    else if (amal == '-') Console.WriteLine($"{a} {amal} {b} = {a - b}");
+    else if (amal == '*') Console.WriteLine($"{a} {amal} {b} = {a * b}");
+    else if (amal == '/')
+    {
+        if (b == 0) Console.WriteLine("Sonni 0 ga bo'lsih mumkun emas");
+        else Console.WriteLine($"{a} {amal} {b} = {a / b}");
+    }
+    else Console.WriteLine("Bunday amal majud emas!!!");
+}
+static void DarajagaKotar()
+{
+    Console.Write("LET'S GO!!! a ning b darajasini hisoblaymiz \n");
+    Console.Write("a = ");
+    double a = double.Parse(Console.ReadLine());
+    Console.Write("b = ");
+    double b = double.Parse(Console.ReadLine());
+    Console.WriteLine($"{a}^{b} = {Math.Pow(a, b)}");
+}
+static void IldizTop()
+{
+    Console.Write("LET'S GO!!! a ning kvadrat ildizini topamiz\n");
+    Console.Write("a = ");
+    double a = double.Parse(Console.ReadLine());
+    if (a < 0) Console.WriteLine("Manfiy sonning kvadrat ildizini topish mumkun emas!!!");
+    Console.WriteLine($"{a} ning kvadrat ildizi {Math.Sqrt(a)} ga teng");
+}
+static void LogarifmHisobla()
+{
+    Console.Write("LET'S GO!!! Logarif a asosga ko'ra b ni hisoblaymiz \n");
+    Console.Write("a = ");
+    double a = double.Parse(Console.ReadLine());
+    Console.Write("b = ");
+    double b = double.Parse(Console.ReadLine());
+    if (a > 0 && a != 1 && b > 0) Console.WriteLine($"log {a} asosga ko'ra {b} ning natijasi {Math.Log(b) / Math.Log(a)} ga teng");
+    else Console.WriteLine("Siz xato son kiritdingiz!!!\na va b nomanfiy sonlar va (a != 1) bo'lishi kerak");
+}
+static void FaktorialHisobla()
+{
+    Console.Write("Qaysi sonning faktorialini hisoblaymiz..?\n a = ");
+    double a = double.Parse(Console.ReadLine());
+    double P = 1;
+    if (a < 0) Console.Write("Manfiy sonning faktorialini hisoblash mumkun emas!!!");
+    else
+    {
+        if (a == 0) Console.WriteLine("0! = 1");
+        else
+        {
+            for (int i = 1; i <= a; i++)
+            {
+                P *= i;
+            }
+            Console.WriteLine($"{a}! = {P}");
+        }
+
+    }
+}
